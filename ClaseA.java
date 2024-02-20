3a03b173aff9008a1cb52ce1560516bad912636c
DScott1223 <diegoscottm@gmail.com>
2024-02-20 12:44:53 +0100

    Versiones clases 3 copia Master

--- ClaseA.java
+++ ClaseA.java
@@ -2,10 +2,10 @@ package git.entornos;
 
 public class ClaseA {
 
-private int aa;
-private int teer;
-  
-private int num4;
-//Codigo de la rama1
-//Codigo de la rama1
+	private int aa;
+	private int teer;
+
+	private int num4;
+	//Codigo de la rama1
+	//Codigo de la rama1
 }
